0f18dcbde06f0e60d54f0130e403f12846b69ad8
lslz627 <lslz627@gmail.com>
2022-09-30 00:51:02 +0800

    print redis src and dst

--- plugSrc/redis/build/entry.go
+++ plugSrc/redis/build/entry.go
@@ -1,41 +1,41 @@
 package build
 
 import (
-	"github.com/google/gopacket"
-	"io"
-	"strings"
-	"fmt"
-	"strconv"
 	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket"
 )
 
 type Redis struct {
-	port int
+	port    int
 	version string
-	cmd chan string
-	done chan bool
+	cmd     chan string
+	done    chan bool
 }
 
 const (
-	Port       int = 6379
+	Port    int    = 6379
 	Version string = "0.1"
 	CmdPort string = "-p"
 )
 
-var redis = &Redis {
-	port:Port,
-	version:Version,
+var redis = &Redis{
+	port:    Port,
+	version: Version,
 }
 
-func NewInstance() *Redis{
+func NewInstance() *Redis {
 	return redis
 }
 
 func (red Redis) ResolveStream(net, transport gopacket.Flow, r io.Reader) {
-
 	buf := bufio.NewReader(r)
-	var cmd string
-	var cmdCount = 0
+	cmdCount := 0
 	for {
 
 		line, _, _ := buf.ReadLine()
@@ -55,43 +55,47 @@ func (red Redis) ResolveStream(net, transport gopacket.Flow, r io.Reader) {
 		}
 
 		//Do not display
-		if strings.EqualFold(transport.Src().String(), strconv.Itoa(red.port)) == true {
+		if strings.EqualFold(transport.Src().String(), strconv.Itoa(red.port)) {
 			continue
 		}
 
+		now := time.Now()
+		// Record time and client address
+		nowStr := now.Format("2006-01-02 15:04:05.000")
+		prefix := fmt.Sprintf("%s [%s:%s -> %s:%s]", nowStr, net.Src().String(), transport.Src().String(), net.Dst().String(), transport.Dst().String())
 		//run
 		l := string(line[1])
+		cmd := ""
 		cmdCount, _ = strconv.Atoi(l)
-		cmd = ""
-		for j := 0; j < cmdCount * 2; j++ {
+		for j := 0; j < cmdCount*2; j++ {
 			c, _, _ := buf.ReadLine()
-			if j & 1 == 0 {
+			if j&1 == 0 {
 				continue
 			}
 			cmd += " " + string(c)
 		}
-		fmt.Println(cmd)
+		fmt.Printf("%s %s\n", prefix, cmd)
 	}
 }
 
 /**
-	SetOption
- */
-func (red *Redis) SetFlag(flg []string)  {
+SetOption
+*/
+func (red *Redis) SetFlag(flg []string) {
 	c := len(flg)
 	if c == 0 {
 		return
 	}
-	if c >> 1 != 1 {
+	if c>>1 != 1 {
 		panic("ERR : Redis num of params")
 	}
-	for i:=0;i<c;i=i+2 {
+	for i := 0; i < c; i = i + 2 {
 		key := flg[i]
 		val := flg[i+1]
 
 		switch key {
 		case CmdPort:
-			port, err := strconv.Atoi(val);
+			port, err := strconv.Atoi(val)
 			redis.port = port
 			if err != nil {
 				panic("ERR : Port error")
@@ -107,16 +111,15 @@ func (red *Redis) SetFlag(flg []string)  {
 }
 
 /**
-	BPFFilter
- */
+BPFFilter
+*/
 func (red *Redis) BPFFilter() string {
-	return "tcp and port "+strconv.Itoa(redis.port)
+	return "tcp and port " + strconv.Itoa(redis.port)
 }
 
 /**
-	Version
- */
+Version
+*/
 func (red *Redis) Version() string {
 	return red.version
 }
-
